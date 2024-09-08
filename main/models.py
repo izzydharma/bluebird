0764bdb97d989f7b32604943972c86c44ae78cdf
izzydharma <147742855+izzydharma@users.noreply.github.com.>
2024-09-09 00:32:54 +0700

    update models

--- main/models.py
+++ main/models.py
@@ -1,12 +1,10 @@
 from django.db import models
-from django.utils import timezone
-
 class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.IntegerField()
     description = models.TextField()
-    rating = models.DecimalField(max_digits=3, decimal_places=2)  # for ratings like 4.5
-    date = models.IntegerField()  # auto-populates with the current date
+    rating = models.DecimalField(max_digits=3, decimal_places=2)  
+    date = models.IntegerField()  
 
     def __str__(self):
         return self.name
