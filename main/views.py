b5feb7acc642255c2a30e2a63402d31a0f3a6658
izzydharma <147742855+izzydharma@users.noreply.github.com.>
2024-09-09 11:46:02 +0700

    adding things

--- main/views.py
+++ main/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 def show_main(request):
     context = {
         'app_name': 'main', 
-        'your_name': 'MadeIzzy Prema Dharma',  
+        'your_name': 'Made Izzy Prema Dharma',  
         'your_class': 'KKI'  
     }
     return render(request, 'main.html', context)
