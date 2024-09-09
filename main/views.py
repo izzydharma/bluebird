daa934b3ebd9acf48c8ec2d45ba62a7ea4040c98
izzydharma <147742855+izzydharma@users.noreply.github.com.>
2024-09-09 11:10:10 +0700

    yang bener

--- main/views.py
+++ main/views.py
@@ -2,28 +2,8 @@ from django.shortcuts import render
 
 def show_main(request):
     context = {
-        'products': [
-            {
-                'name': 'USB C charger',
-                'price': 5,
-                'description': 'Charger with braided cable',
-                'rating': 4.5,
-                'date': '2024-09-06'
-            },
-            {
-                'name': 'Laptop',
-                'price': 79,
-                'description': 'Great laptop for everyday use',
-                'rating': 4.2,
-                'date': '2024-09-05'
-            },
-            {
-                'name': 'Computer',
-                'price': 300,
-                'description': 'Great computer for gaming',
-                'rating': 4.8,
-                'date': '2024-09-04'
-            }
-        ]
+        'app_name': 'Bluebird', 
+        'your_name': 'MadeIzzy Prema Dharma',  
+        'your_class': 'KKI'  
     }
     return render(request, 'main.html', context)
