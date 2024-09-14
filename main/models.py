cc7a8ad96075d7ab4a4f7b514380384e243b65ff
izzydharma <147742855+izzydharma@users.noreply.github.com.>
2024-09-14 16:25:37 +0700

    assignment 3

--- main/models.py
+++ main/models.py
@@ -1,10 +1,13 @@
 from django.db import models
+import uuid
+
 class Product(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=100)
     price = models.IntegerField()
     description = models.TextField()
     rating = models.DecimalField(max_digits=3, decimal_places=2)  
-    date = models.IntegerField()  
+    date = models.DateField(auto_now_add=True) 
 
     def __str__(self):
         return self.name
