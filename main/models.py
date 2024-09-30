66859f5bb638d8e035a39a0f28c2eab184deb936
izzydharma <147742855+izzydharma@users.noreply.github.com.>
2024-10-01 00:33:58 +0700

    fixing assignment

--- main/models.py
+++ main/models.py
@@ -1,15 +1,18 @@
 from django.db import models
 import uuid
 from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 class Product(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)    
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=100)
     price = models.IntegerField()
     description = models.TextField()
-    rating = models.DecimalField(max_digits=3, decimal_places=2)  
-    date = models.DateField(auto_now_add=True) 
+    rating = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(10)]
+    )
+    date = models.DateField(auto_now_add=True)
 
     def __str__(self):
         return self.name
